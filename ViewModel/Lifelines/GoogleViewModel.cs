02f8d89267349037df1e2df8393b72073cb81bdb
Paul Lopatin <paul.a.lopatin@gmail.com>
2021-12-17 13:57:48 +0300

    Добавление базового ViewModel для подсказок с таймером. Добавление подсказки «Помощь Интернета».

---
+++ ViewModel/Lifelines/GoogleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WwtbamOld.Model;
+
+namespace WwtbamOld.ViewModel
+{
+    public sealed class GoogleViewModel : TimerLifelineViewModel
+    {
+        public GoogleViewModel(LifelinesViewModel lifelinesViewModel, Google lifeline)
+            : base(lifelinesViewModel, lifeline)
+        {
+        }
+    }
+}
--- ViewModel/Lifelines/PhoneAFriendViewModel.cs
+++ ViewModel/Lifelines/PhoneAFriendViewModel.cs
@@ -1,50 +1,12 @@
-﻿using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using System;
-using System.Reactive;
-using System.Reactive.Linq;
-using WwtbamOld.Model;
+﻿using WwtbamOld.Model;
 
 namespace WwtbamOld.ViewModel
 {
-    public sealed class PhoneAFriendViewModel : LifelineViewModel<PhoneAFriend>
+    public sealed class PhoneAFriendViewModel : TimerLifelineViewModel
     {
-        #region Fields
-
-        private readonly ObservableAsPropertyHelper<uint> _countdown;
-
-        #endregion Fields
-
-        public PhoneAFriendViewModel(LifelinesViewModel lifelinesViewModel, PhoneAFriend lifeline) : base(lifelinesViewModel, lifeline)
+        public PhoneAFriendViewModel(LifelinesViewModel lifelinesViewModel, PhoneAFriend lifeline)
+            : base(lifelinesViewModel, lifeline)
         {
-            this.WhenAnyValue(vm => vm._model.Duration).Select(x => (uint)x.TotalSeconds).BindTo(this, x => x.Duration);
-            this.WhenAnyValue(vm => vm.Duration).Select(x => TimeSpan.FromSeconds(x)).BindTo(_model, x => x.Duration);
-
-            this.WhenAnyValue(vm => vm._model.IsTimerShown).BindTo(this, x => x.IsTimerShown);
-            this.WhenAnyValue(vm => vm.IsTimerShown).BindTo(_model, x => x.IsTimerShown);
-
-            _countdown = this.WhenAnyValue(vm => vm._model.RemainingTime).Select(x => (uint)x.TotalSeconds).ToProperty(this, nameof(Countdown));
-
-            InitiateCommand = ReactiveCommand.Create(() => _model.Initiate());
-            StopCommand = ReactiveCommand.Create(() => _model.Stop());
-            EndCommand = ReactiveCommand.Create(() => _model.End());
         }
-
-        #region Properties
-
-        [Reactive] public uint Duration { get; set; }
-        public uint Countdown => _countdown != null ? _countdown.Value : 0;
-
-        [Reactive] public bool IsTimerShown { get; set; }
-
-        #endregion Properties
-
-        #region Commands
-
-        public ReactiveCommand<Unit, Unit> InitiateCommand { get; }
-        public ReactiveCommand<Unit, Unit> StopCommand { get; }
-        public ReactiveCommand<Unit, Unit> EndCommand { get; }
-
-        #endregion Commands
     }
 }
